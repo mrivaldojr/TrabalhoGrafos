e6242e5448e350634f8660481b426c500aaa4942
junior <mrivaldojr@gmail.com>
2014-11-30 17:11:12 -0300

    Commit inicial começando a fazer estruturas

---
+++ src/grafo/Grafo.java
@@ -0,0 +1,9 @@
+package grafo;
+
+import java.util.ArrayList;
+
+public class Grafo {
+	
+	private ArrayList<Vertice> vertices;
+	private ArrayList<Aresta> arestas;
+}
---
+++ src/grafo/Vertice.java
@@ -0,0 +1,11 @@
+package grafo;
+
+import java.awt.List;
+
+
+public class Vertice {
+	
+	private List verticesAdjacentes;
+	private int grau;
+	
+}
